938d380fa12797ade1ca0942be6878580d05dfd5
liao-lingfeng <f15079447849@163.com>
2022-02-14 22:40:10 +0800

    ribbon

---
+++ cloud-provider-payment8004/src/main/java/com/feng/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.feng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author
+ * @time 2022/1/26 21:30
+ * @Description- TODO
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
